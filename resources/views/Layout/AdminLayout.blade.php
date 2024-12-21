16321319f38315e4f2299b2127420176e0af2151
Nasrul Fahmi <75nasrulfahmi@gmail.com>
2024-12-21 16:17:23 +0700

    test

--- resources/views/Layout/AdminLayout.blade.php
+++ resources/views/Layout/AdminLayout.blade.php
@@ -8,7 +8,7 @@
     <script src="https://unpkg.com/feather-icons"></script>
     <script src="https://kit.fontawesome.com/3197b77032.js" crossorigin="anonymous"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <title>Form Pengisian Data Instansi | Lobi Poliwangi</title>
+    <title>dawduawdawdawd Form Pengisian Data Instansi | Lobi Poliwangi</title>
 </head>
 
 <body class=" font-Poppins">
