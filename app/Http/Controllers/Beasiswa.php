0b0ae811ca730a375cb80b77b98a337e79201ba1
rahmahsf <rahmahsafa06@gmail.com>
2025-01-06 22:33:17 +0700

    edit instansi lomba and beasiswa

--- app/Http/Controllers/Beasiswa.php
+++ app/Http/Controllers/Beasiswa.php
@@ -105,8 +105,7 @@ class Beasiswa extends Controller
         return redirect(route('Beasiswa.Index'));
     }
 
-    public function EditBeasiswa(Request $request)
-    {
+    public function EditBeasiswa(Request $request) {
         $BeasiswaRelation = ['Tingkatan', 'Instansi', 'Status', 'Admin'];
 
         $DataBeasiswa = dbBeasiswa::find($request->IdObject)->load($BeasiswaRelation);
@@ -115,7 +114,7 @@ class Beasiswa extends Controller
 
         $PendaftaranParshed = Carbon::createFromFormat('d-m-Y', $DataBeasiswa->tanggal_pendaftaran)->format('Y-m-d');
         $PenutupanParshed = Carbon::createFromFormat('d-m-Y', $DataBeasiswa->tanggal_penutupan)->format('Y-m-d');
-        return view('admin.beasiswa.EditData', [
+        return view('admin.beasiswa.EditData',[
             'DataBeasiswa' => $DataBeasiswa,
             'TanggalPendaftaran' => $PendaftaranParshed,
             'TanggalPenutupan' => $PenutupanParshed,
@@ -124,8 +123,7 @@ class Beasiswa extends Controller
         ]);
     }
 
-    public function UpdateBeasiswa(Request $request)
-    {
+    public function UpdateBeasiswa(Request $request) {
         $ValidateData = $request->validate([
             'IdInstansi' => ['required'],
             'NamaBeasiswa' => ['required', 'max:100', 'min:3'],
@@ -134,7 +132,7 @@ class Beasiswa extends Controller
             'TanggalPenutupan' => ['required'],
             'Tingkatan' => ['required'],
             'Persyaratan' => ['required'],
-            'FotoBeasiswa' => ['nullable', 'image', 'mimes:jpeg,jpg,png', 'file', 'max:5120'],
+            'FotoBeasiswa' => ['nullable', 'image', 'mimes:jpeg,jpg,png','file', 'max:5120'],
         ]);
         $ValidateData['Administrator'] = auth('Admin')->user()->id;
         $DataPicker = dbBeasiswa::find($request->IdBeasiswa);
@@ -149,16 +147,14 @@ class Beasiswa extends Controller
             'status_id' => 1,
             'admin_id' => $ValidateData['Administrator'],
         ]);
-        if ($request->has('FotoBeasiswa')) {
-            $ValidateData['FotoBeasiswa'] = $request->file('FotoBeasiswa')->store('\Beasiswa', 'public');
+        if($request->has('FotoBeasiswa')){
+            $ValidateData['FotoBeasiswa']=$request->file('FotoBeasiswa')->store('\Beasiswa\EventImg');
             $DataPicker->update([
                 'foto_beasiswa' => $ValidateData['FotoBeasiswa'],
             ]);
         }
         return redirect(route('Beasiswa.Detail', ['id' => $DataPicker]));
-    }
-
-    public function RestoreBeasiswa(Request $request)
+    }    public function RestoreBeasiswa(Request $request)
     {
         $DataPicker = dbBeasiswa::withTrashed()->find($request->IdBeasiswa);
         $DataPicker->restore();
